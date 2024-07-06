a895c70742dea55301ba3ce946455e818dfa885d
Aditya Khera <adityakhera@Adityas-MacBook-Pro.local>
2024-07-06 10:43:38 +0100

    Added Edit Page Functionality

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('wiki/<str:title>', views.getentry, name = 'getentry'),
     path("search", views.search, name="search"),
     path("create", views.createentry, name='create'),
+    path("wiki/edit/<str:title>", views.editpage, name="edit")
 ]
